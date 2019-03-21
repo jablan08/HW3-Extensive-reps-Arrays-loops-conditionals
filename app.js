1509840d28c9ffda20c7e081b37b186c47e35412
Jablan <joshuaablan@gmail.com>
2019-03-20 17:35:13 -0700

    Commit4 - Fizz Buzz answered

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
+// Easy going
 for (let i = 1; i <= 20; i++) {
     console.log(i);   
 }
-
+// Get even
 for (let i = 0; i <= 200; i++) {
     if (i % 2 ===0) {
         console.log(i);
     }
 }
 
+// Excited Kitten
+// #1
+for (let i = 0; i < 20; i++) {
+    console.log("Love me, pet me! HSSSSSS!")
+}
+// #2
 const arr = ["...human...why you taking pictures of me?...",
  "...the catnip made me do it...", "...why does the red dot always get away..."];
 for (let i = 0; i < 20; i++) {
@@ -17,4 +24,35 @@ for (let i = 0; i < 20; i++) {
         console.log(arr[random]);
     };
 };
-   
+
+// Fizz Buzz
+const arr1 = ["Fizz", "Buzz", "FizzBuzz"];
+ for (let i = 0; i <= 100; i++) {
+     if (i % 3 ===0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+     }
+     else if (i % 5 === 0) {
+         console.log("Buzz");
+     }
+     else if (i % 3 ===0) {
+        console.log("Fizz");
+     }
+     else {
+         console.log(i);
+     };
+ };
+
+// Getting to Know You
+
+// Yell at Ninja Turtles
+
+
+//Return of the Closets
+
+// Dirty Laundry
+
+// Inventory
+
+
+// Multiple of 3 and 5
+
